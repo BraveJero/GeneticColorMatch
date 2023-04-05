fc59e59fd32d5e9f597c731a548c71e14f69293f
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2023-04-05 18:41:43 -0300

    add rank and universal selection

--- src/selection_method.py
+++ src/selection_method.py
@@ -34,15 +34,23 @@ class EliteSelection(SelectionMethod):
 class RouletteWheelSelection(SelectionMethod):
     def get_winners(self, population: List[Individual], k: int, fitness: Callable[[Individual], float]) \
             -> List[Individual]:
-        individual_fitness_list = [fitness(indv) for indv in population]
-        fitness_sum = sum(individual_fitness_list)
-        relative_fitness = [individual_fitness / fitness_sum for individual_fitness in individual_fitness_list]
+        length = len(population)
+        if length == 0:
+            return []
 
+        individual_fitness_list = [fitness(ind) for ind in population]
+        return self.do_roulette(population, length, k, individual_fitness_list)
+
+    def do_roulette(self, population: List[Individual], length: int, k: int, fitness_list: List[float]):
         winners = []
+
+        fitness_sum = sum(fitness_list)
+        relative_fitness = [individual_fitness / fitness_sum for individual_fitness in fitness_list]
+
         for i in range(k):
             r = random.random()
             accumulated_fitness = 0
-            for j in range(len(population)):
+            for j in range(length):
                 if accumulated_fitness < r <= accumulated_fitness + relative_fitness[j]:
                     winners.append(population[j])
                     break
@@ -54,13 +62,40 @@ class RouletteWheelSelection(SelectionMethod):
 class UniversalSelection(SelectionMethod):
     def get_winners(self, population: List[Individual], k: int, fitness: Callable[[Individual], float]) \
             -> List[Individual]:
-        pass
+        winners = []
+
+        length = len(population)
+        if length == 0:
+            return winners
+
+        individual_fitness_list = [fitness(ind) for ind in population]
+        fitness_sum = sum(individual_fitness_list)
+        relative_fitness = [individual_fitness / fitness_sum for individual_fitness in individual_fitness_list]
+
+        r = random.random()
+        for i in range(k):
+            ri = (r + i) / k
+            accumulated_fitness = 0
+            for j in range(length):
+                if accumulated_fitness < ri <= accumulated_fitness + relative_fitness[j]:
+                    winners.append(population[j])
+                    break
+                accumulated_fitness += relative_fitness[j]
+
+        return winners
 
 
-class RankSelection(SelectionMethod):
+class RankSelection(RouletteWheelSelection):
     def get_winners(self, population: List[Individual], k: int, fitness: Callable[[Individual], float]) \
             -> List[Individual]:
-        pass
+        length = len(population)
+        if length == 0:
+            return []
+
+        population.sort(key=lambda ind: fitness(ind))
+        individual_fitness_list = [(length - (idx + 1)) / length for idx, ind in enumerate(population)]
+
+        return self.do_roulette(population, length, k, individual_fitness_list)
 
 
 class EntropicBoltzmannSelection(SelectionMethod):
