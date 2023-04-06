365cd14b8fea909b42f8c9a4a0eb97245282f6eb
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2023-04-06 12:57:36 -0300

    fix array index out of bounds

--- src/selection_method.py
+++ src/selection_method.py
@@ -80,16 +80,15 @@ class DeterministicTournamentSelection(SelectionMethod):
             -> List[Individual]:
         winners = []
 
+        length = len(population)
         if len(population) == 0:
             return winners
 
-        # TODO: Check repeated winners?
-
         for i in range(k):
-            best = population[random.randint(0, len(population))]
+            best = population[random.randint(0, length - 1)]
             best_fitness = fitness(best)
             for j in range(1, self._m):
-                chosen = population[random.randint(0, len(population))]
+                chosen = population[random.randint(0, length - 1)]
                 aux_fitness = fitness(chosen)
                 if best_fitness < aux_fitness:
                     best = chosen
@@ -112,8 +111,8 @@ class ProbabilisticTournamentSelection(SelectionMethod):
         length = len(population)
 
         for i in range(k):
-            first = population[random.randint(0, length)]
-            second = population[random.randint(0, length)]
+            first = population[random.randint(0, length - 1)]
+            second = population[random.randint(0, length - 1)]
             first_fitness = fitness(first)
             second_fitness = fitness(second)
 
