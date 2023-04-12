e0675e6e51ea9fa66d64f8c1882ce69d268e20bc
shoni <juanigarcia@itba.edu.ar>
2023-04-11 23:21:54 -0300

    Update Individual to keep a copy of its chromosomes

--- src/individual.py
+++ src/individual.py
@@ -1,5 +1,13 @@
 from abc import ABC
+from .chromosome import Chromosome, ColorChromosome
 
 
 class Individual(ABC):
-    pass
+    def __init__(self, chromosome: Chromosome):
+        pass
+
+
+class ColorIndividual(Individual):
+    def __init__(self, chromosome: ColorChromosome):
+        self._chromosome = chromosome
+
