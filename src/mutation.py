82f26a669a2918e84a7488b265b12aa3777b055e
shoni <juanigarcia@itba.edu.ar>
2023-04-11 23:35:11 -0300

    Add abstract class for mutation

---
+++ src/mutation.py
@@ -0,0 +1,16 @@
+from abc import ABC
+
+from .chromosome import Chromosome, ColorChromosome
+
+
+class Mutation(ABC):
+    def mutate(self, chromosome: Chromosome, dev: float, prob: float):
+        """
+        :param chromosome: chromosome tu mutate
+        :param dev: each allele mutates according to a Gaussian bell centered at its value with deviation dev
+        :param prob: probability of mutating an allele to some othe random
+        """
+        pass
+
+
+
