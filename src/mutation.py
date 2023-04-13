9a08e3df749621fee23c273bd50cd45d2aded056
shoni <juanigarcia@itba.edu.ar>
2023-04-13 02:30:33 -0300

    Add mutation methods

--- src/mutation.py
+++ src/mutation.py
@@ -1,16 +1,41 @@
 from abc import ABC
 
-from .chromosome import Chromosome, ColorChromosome
+from .chromosome import Chromosome
+from .gene import Gene
+from .mutation_method import MutationMethod
+import numpy as np
+import random
 
 
 class Mutation(ABC):
-    def mutate(self, chromosome: Chromosome, dev: float, prob: float):
-        """
-        :param chromosome: chromosome to mutate
-        :param dev: each allele mutates according to a Gaussian bell centered at its value with deviation dev
-        :param prob: probability of mutating an allele to some other random
-        """
+    def __init__(self, prob: float, method: MutationMethod):
+        if prob < 0 or prob > 1:
+            raise ValueError("Wrong probability")
+        self._prob = prob
+        self._method = method
+
+    def mutate(self, ch: Chromosome):
         pass
 
 
+class SingleGeneMutation(Mutation):
+    def mutate(self, ch: Chromosome):
+        if np.random.uniform(0, 1) < self._prob:
+            index = np.random.randint(0, len(ch.information))
+            self._method(ch.information[index])
 
+
+class LimitedMultigeneMutation(Mutation):
+    def mutate(self, ch: Chromosome):
+        M = np.random.randint(0, len(ch.information))
+        indexes = random.sample(range(len(ch.information)), M)
+        for i in indexes:
+            if np.random.uniform(0, 1) < self._prob:
+                self._method(ch.information[i])
+
+
+class UniformMutation(Mutation):
+    def mutate(self, ch: Chromosome):
+        for g in ch.information:
+            if np.random.uniform(0, 1) < self._prob:
+                self._method(g)
