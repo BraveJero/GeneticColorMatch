8be5c6462c8f16616d2047539374bd3f7ae48a9f
shoni <juanigarcia@itba.edu.ar>
2023-04-13 02:37:37 -0300

    Add init to parent class

--- src/individual.py
+++ src/individual.py
@@ -1,13 +1,18 @@
+from __future__ import annotations
+
 from abc import ABC
-from .chromosome import Chromosome, ColorChromosome
+from .chromosome import Chromosome
+from .color_palette import ColorPalette
+from .color import Color
 
 
 class Individual(ABC):
     def __init__(self, chromosome: Chromosome):
-        pass
+        self._chromosome = chromosome
 
 
 class ColorIndividual(Individual):
-    def __init__(self, chromosome: ColorChromosome):
+    def __init__(self, chromosome: Chromosome, palette: ColorPalette, goal: Color):
+        super(chromosome)
         self._chromosome = chromosome
-
+        self._palette
