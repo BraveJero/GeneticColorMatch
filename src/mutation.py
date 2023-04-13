44cd9457aa7768e3f9e90d3a91b1901669f5be1f
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-04-12 21:33:01 -0300

    Fix typo

--- src/mutation.py
+++ src/mutation.py
@@ -6,7 +6,7 @@ from .chromosome import Chromosome, ColorChromosome
 class Mutation(ABC):
     def mutate(self, chromosome: Chromosome, dev: float, prob: float):
         """
-        :param chromosome: chromosome tu mutate
+        :param chromosome: chromosome to mutate
         :param dev: each allele mutates according to a Gaussian bell centered at its value with deviation dev
         :param prob: probability of mutating an allele to some othe random
         """
