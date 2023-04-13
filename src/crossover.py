fa697e2460b68910e02280ba5a8431021fb0a548
shoni <juanigarcia@itba.edu.ar>
2023-04-12 23:12:39 -0300

    Add uniform crossover

--- src/crossover.py
+++ src/crossover.py
@@ -12,54 +12,61 @@ def validate_length(par1: Chromosome, par2: Chromosome) -> int:
     return len(par1.information)
 
 
-def swap_sublist(l1: List, l2: List, left: int, right: int):
-    for i in range(left, right):
-        l1[i], l2[i] = l2[i], l1[i]
+def swap_sublist(l1: List, l2: List, left: int, right: int) -> Tuple[List, List]:
+    ch1, ch2 = [None] * len(l1), [None] * len(l2)
+    for i in range(len(l1)):
+        ch1[i] = l1[i] if i < left or i > right else l2[i]
+        ch2[i] = l2[i] if i < left or i > right else l1[i]
+    return ch1, ch2
 
 
 class Crossover(ABC):
-    @staticmethod
+    def __init__(self, uniform=0.5):
+        self._uniform = uniform
+
     @abstractmethod
-    def cross(par1: Chromosome, par2: Chromosome) -> Tuple[List, List]:
+    def cross(self, par1: Chromosome, par2: Chromosome) -> Tuple[List, List]:
         pass
 
 
 class OnePointCrossover(Crossover):
-    @staticmethod
-    def cross(par1: Chromosome, par2: Chromosome) -> Tuple[List, List]:
+    def cross(self, par1: Chromosome, par2: Chromosome) -> Tuple[List, List]:
         length = validate_length(par1, par2)
-        ch1 = [None] * length
-        ch2 = [None] * length
         p = np.random.randint(0, length)
-        for i in range(length):
-            ch1[i] = par1.information[i] if i < p else par2.information[i]
-            ch2[i] = par2.information[i] if i < p else par1.information[i]
-        return ch1, ch2
+        return swap_sublist(par1.information, par2.information, p, length - 1)
 
 
 class TwoPointCrossover(Crossover):
-    @staticmethod
-    def cross(par1: Chromosome, par2: Chromosome) -> Tuple[List, List]:
+    def cross(self, par1: Chromosome, par2: Chromosome) -> Tuple[List, List]:
         length = validate_length(par1, par2)
-        ch1 = [None] * length
-        ch2 = [None] * length
-        p1 = np.random.randint(0, length)
-        p2 = np.random.randint(p1, length)
-        for i in range(length):
-            ch1[i] = par1.information[i] if i < p1 or i > p2 else par2.information[i]
-            ch2[i] = par2.information[i] if i < p1 or i > p2 else par1.information[i]
-        return ch1, ch2
+        left = np.random.randint(0, length)
+        right = np.random.randint(left, length)
+        return swap_sublist(par1.information, par2.information, left, right)
 
 
 class AnnularCrossover(Crossover):
-    @staticmethod
-    def cross(par1: Chromosome, par2: Chromosome) -> Tuple[List, List]:
+    def cross(self, par1: Chromosome, par2: Chromosome) -> Tuple[List, List]:
         length = validate_length(par1, par2)
-        ch1 = [None] * length
-        ch2 = [None] * length
-        p = 2  # np.random.randint(0, length)
-        length = 2  # np.random.randint(0, math.ceil(length/2))
-        p1 = math.min(p, (p + length) % length)
-        p2 = p1 + length
-        swap_sublist(ch1, ch2, p1, p2)
+        p = np.random.randint(0, length)
+        L = np.random.randint(0, math.ceil(length / 2))
+        if L == 0:
+            left = 0
+            right = length
+        elif p + L < length:
+            left = p
+            right = p + L - 1
+        else:
+            left = (p + L) % length
+            right = p - 1
+        return swap_sublist(par1.information, par2.information, left, right)
+
+
+class UniformCrossover(Crossover):
+    def cross(self, par1: Chromosome, par2: Chromosome) -> Tuple[List, List]:
+        length = validate_length(par1, par2)
+        ch1, ch2 = [None] * length, [None] * length
+        for i in range(length):
+            p = np.random.uniform(0, 1)
+            ch1[i] = par1.information[i] if p < self._uniform else par2.information[i]
+            ch2[i] = par2.information[i] if p < self._uniform else par1.information[i]
         return ch1, ch2
