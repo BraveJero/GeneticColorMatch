cdee4aadf01b8fce966e9698c4f20bd7ba8ca77e
shoni <juanigarcia@itba.edu.ar>
2023-04-13 02:28:48 -0300

    Change implementation to use Gene class

--- src/chromosome.py
+++ src/chromosome.py
@@ -1,33 +1,17 @@
 from __future__ import annotations
 
-from abc import ABC
 from copy import copy
 from typing import List
-from math import gcd
+from .gene import Gene
 
 
-class Chromosome(ABC):
-    def __init__(self, information: List):
-        pass
-
-    @property
-    def information(self) -> List:
-        pass
-
-
-class ColorChromosome(Chromosome):
-    def __init__(self, proportions: List[int]):
-        self._proportions = copy(proportions)  # TODO: check if deepcopy is necessary
-        self._simplify()
+class Chromosome:
+    def __init__(self, information: List[Gene]):
+        self._information = copy(information)
 
     @property
-    def information(self):
-        return self._proportions
-
-    # Simplifies proportions to simplest terms (i.e. [2, 4, 6] ---> [1, 2, 3])
-    def _simplify(self):
-        d = gcd(*self._proportions)
-        self._proportions = [p // d for p in self._proportions]
+    def information(self) -> List[Gene]:
+        return self._information
 
     def __repr__(self):
-        return self._proportions.__repr__()
+        return self._information.__repr__()
