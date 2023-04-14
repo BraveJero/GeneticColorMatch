51df7e69fdbf26b56419e60ecf8dd78536610469
shoni <juanigarcia@itba.edu.ar>
2023-04-14 12:03:45 -0300

    Add basic stop condition

--- src/stop_condition.py
+++ src/stop_condition.py
@@ -1,7 +1,18 @@
 from abc import ABC, abstractmethod
+from typing import List
+
+from .individual import Individual
 
 
 class StopCondition(ABC):
     @abstractmethod
-    def __call__(self):
+    def __call__(self, generation_count: int, generation: List[Individual]) -> bool:
         pass
+
+
+class GenerationCountStopCondition(StopCondition):
+    def __init__(self, count: int):
+        self._count = count
+
+    def __call__(self, generation_count: int, generation: List[Individual]) -> bool:
+        return generation_count >= self._count
