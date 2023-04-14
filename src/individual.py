43f6f87ef3954a4780a6ab329acdefb748cb8de7
shoni <juanigarcia@itba.edu.ar>
2023-04-14 12:20:29 -0300

    Add __repr__ and fitness

--- src/individual.py
+++ src/individual.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from abc import ABC
+from abc import ABC, abstractmethod
 from .chromosome import Chromosome
 from .color_palette import ColorPalette
 from .color import Color
@@ -10,9 +10,28 @@ class Individual(ABC):
     def __init__(self, chromosome: Chromosome):
         self._chromosome = chromosome
 
+    @abstractmethod
+    def fitness(self) -> float:
+        pass
+
+    @property
+    def chromosome(self) -> Chromosome:
+        return self._chromosome
+
 
 class ColorIndividual(Individual):
     def __init__(self, chromosome: Chromosome, palette: ColorPalette, goal: Color):
-        super(chromosome)
+        super().__init__(chromosome)
         self._chromosome = chromosome
-        self._palette
+        self._palette = palette
+        self._goal = goal
+        self._color = palette.from_proportions([g.value for g in self._chromosome.information])
+
+    def fitness(self) -> float:
+        return Color.distance(self._goal, self._color)
+
+    def __str__(self):
+        return self._color.__str__()
+
+    def __repr__(self):
+        return self.__str__()
