afd766c54255eb9cf46cace9d3d405f413ee3e32
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2023-04-15 14:12:00 -0300

    add type anotations to fix warinigs

--- src/crossover.py
+++ src/crossover.py
@@ -1,9 +1,11 @@
+import math
 from abc import ABC, abstractmethod
 from typing import Tuple, List
 
-from .chromosome import Chromosome
 import numpy as np
-import math
+
+from .chromosome import Chromosome
+from .gene import Gene
 
 
 def validate_length(par1: Chromosome, par2: Chromosome) -> int:
@@ -12,8 +14,9 @@ def validate_length(par1: Chromosome, par2: Chromosome) -> int:
     return len(par1.information)
 
 
-def swap_sublist(l1: List, l2: List, left: int, right: int) -> Tuple[Chromosome, Chromosome]:
-    ch1, ch2 = [None] * len(l1), [None] * len(l2)
+def swap_sublist(l1: List[Gene], l2: List[Gene], left: int, right: int) -> Tuple[Chromosome, Chromosome]:
+    ch1: List[Gene | None] = [None] * len(l1)
+    ch2: List[Gene | None] = [None] * len(l2)
     for i in range(len(l1)):
         ch1[i] = l1[i] if i < left or i > right else l2[i]
         ch2[i] = l2[i] if i < left or i > right else l1[i]
@@ -64,7 +67,8 @@ class AnnularCrossover(Crossover):
 class UniformCrossover(Crossover):
     def cross(self, par1: Chromosome, par2: Chromosome) -> Tuple[Chromosome, Chromosome]:
         length = validate_length(par1, par2)
-        ch1, ch2 = [None] * length, [None] * length
+        ch1: List[Gene | None] = [None] * length
+        ch2: List[Gene | None] = [None] * length
         for i in range(length):
             p = np.random.uniform(0, 1)
             ch1[i] = par1.information[i] if p < self._uniform else par2.information[i]
