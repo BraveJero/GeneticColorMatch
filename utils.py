6f308c91e5180e3f0eec9ad35ddfbf206d4da20a
BraveJero <jbrave271@gmail.com>
2023-04-16 19:42:55 -0300

    update plot info

--- utils.py
+++ utils.py
@@ -3,9 +3,13 @@ import numpy as np
 from PIL import Image
 from tqdm import tqdm
 
+FRAME_DURATION = 150  # ms
+ROTATION_RATIO = 1  # Degrees per FRAME_DURATION
+INITIAL_ROTATION = 180  # Degrees
+ELEVATION = 30  # View elevation in degrees
 
-# Define a function to create a 3D scatter plot with normalized RGB colors
-def create_3d_plot(colors, color_goal):
+
+def create_3d_plot(colors, color_goal, rotation):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     r, g, b = np.array(colors).T
@@ -16,6 +20,8 @@ def create_3d_plot(colors, color_goal):
     ax.set_xlim(0, 1)
     ax.set_ylim(0, 1)
     ax.set_zlim(0, 1)
+    rotation[0] = (rotation[0] + ROTATION_RATIO) % 360
+    ax.view_init(azim=rotation[0], elev=ELEVATION)
 
     ax.scatter([color_goal.r / 255], [color_goal.g / 255], [color_goal.b / 255], c='r', marker='o')
 
@@ -23,18 +29,18 @@ def create_3d_plot(colors, color_goal):
 
 
 def plot_data(colors_list, color_goal):
-    # Create a list of figures to be saved as frames in the GIF
     print("Plot progress")
     frames = []
+    rotation = [INITIAL_ROTATION]
     for i, colors in tqdm(enumerate(colors_list), total=len(colors_list)):
-        fig = create_3d_plot(colors, color_goal)
+        fig = create_3d_plot(colors, color_goal, rotation)
         plt.title(f"Generation {i}")
         fig.canvas.draw()
-        # Get the RGBA array of the figure
         frame = np.array(fig.canvas.renderer.buffer_rgba())
         frames.append(Image.fromarray(frame))
-        plt.close(fig)  # Close the figure to prevent it from displaying in the notebook
+        plt.close(fig)
 
-    # Save the frames as a GIF
-    frames[0].save('generational_point_plot.gif', format='GIF', append_images=frames[1:], save_all=True, duration=100,
+        # Save the frames as a GIF
+    frames[0].save('generational_point_plot.gif', format='GIF', append_images=frames[1:], save_all=True,
+                   duration=FRAME_DURATION,
                    loop=0)
