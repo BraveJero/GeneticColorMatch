cb49738dee35fbadf3b13adddad569cea3df33a5
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2023-04-18 00:48:15 -0300

    Implement line graph with bar errors

--- plot.py
+++ plot.py
@@ -1,6 +1,8 @@
 import os
 from abc import ABC
 from functools import reduce
+from numbers import Number
+from statistics import mean, stdev
 from typing import List
 
 import matplotlib.pyplot as plt
@@ -15,12 +17,12 @@ from src.individual import Individual
 from src.individual_factory import ColorProportionIndividualFactory
 from src.mutation import UniformMutation
 from src.mutation_method import ColorProportionRandomMutation
-from src.selection_method import EliteSelection
+from src.selection_method import EliteSelection, RankSelection, UniversalSelection, DeterministicTournamentSelection
 from src.simulation import Simulation
 from src.stop_condition import GenerationCountStopCondition
 
-TEST_COUNT = 10
-GENERATION_COUNT = 5
+TEST_COUNT = 50
+GENERATION_COUNT = 100
 PALETTE_SIZE = 8
 GENERATION_SIZE = 10
 CHILDREN_SIZE = 5
@@ -28,47 +30,58 @@ CHILDREN_SIZE = 5
 OUTPUT_DIR = "figs/"
 
 
-def _get_random_color() -> Color:
-    return Color(
-        np.random.randint(0, 255),
-        np.random.randint(0, 255),
-        np.random.randint(0, 255)
-    )
+GOAL_COLOR: Color = Color(255//2, 255//2, 255//2)
+
+COLOR_PALETTE: ColorPalette = ColorPalette([
+    Color(0, 0, 0),
+    Color(0, 0, 255),
+    Color(0, 255, 0),
+    Color(0, 255, 255),
+    Color(255, 0, 0),
+    Color(255, 0, 255),
+    Color(255, 255, 0),
+    Color(255, 255, 255),
+])
 
 
 class Plotter(ABC):
     def plot(self):
-        color_goal: Color = _get_random_color()
-        colors: List[Color] = []
-        for _ in range(PALETTE_SIZE):
-            colors.append(_get_random_color())
-        color_palette: ColorPalette = ColorPalette(colors)
-        s: Simulation = Simulation(
-            n=GENERATION_SIZE,
-            k=CHILDREN_SIZE,
-            stop_condition=GenerationCountStopCondition(GENERATION_COUNT),
-            selection_method=EliteSelection(),
-            crossover_method=OnePointCrossover(),
-            mutation=UniformMutation(0.1, ColorProportionRandomMutation()),
-            individual_factory=ColorProportionIndividualFactory(goal=color_goal, palette=color_palette),
-            generation_selection=UseAllGenerationSelection(EliteSelection(), GENERATION_SIZE)
-        )
+        ans = []
+        for _ in range(TEST_COUNT):
+            s: Simulation = Simulation(
+                n=GENERATION_SIZE, # 10
+                k=CHILDREN_SIZE, # 5
+                stop_condition=GenerationCountStopCondition(GENERATION_COUNT),
+                selection_method=DeterministicTournamentSelection(5),
+                crossover_method=OnePointCrossover(),
+                mutation=UniformMutation(0.001, ColorProportionRandomMutation()),
+                individual_factory=ColorProportionIndividualFactory(goal=GOAL_COLOR, palette=COLOR_PALETTE),
+                generation_selection=UseAllGenerationSelection(EliteSelection(), GENERATION_SIZE)
+            )
 
-        generations: List[List[Individual]] = s.simulate()
-        fitnesses = [
-            [individual.fitness() for individual in generation] for generation in generations
+            generations: List[List[Individual]] = s.simulate()
+            ans.append(generations)
+
+        # ans: List[List[List[Individuals]]]
+        list_of_max_fitness: List[List[Number]] = [
+            [max([individual.fitness() for individual in generation]) for generation in simulation] for simulation in
+            ans
         ]
-        bests = list(map(
-            lambda g: reduce(
-                (lambda i1, i2: i1 if i1.fitness() >= i2.fitness() else i2),
-                g
-            ),
-            generations
-        ))
-        plt.plot(range(len(bests)), list(map(lambda i: i.fitness(), bests)))
-        print(list(map(lambda i: i.fitness(), bests)), sep="\n")
+        means: List[Number] = [
+            mean([list_of_max_fitness[test][generation] for test in range(TEST_COUNT)]) for generation in
+            range(GENERATION_COUNT)
+        ]
+        stds: List[Number] = [
+            stdev([list_of_max_fitness[test][generation] for test in range(TEST_COUNT)]) for generation in
+            range(GENERATION_COUNT)
+        ]
+        plt.errorbar(range(len(means)), means, yerr=stds, label='')
         plt.ylim([0, MAX_FITNESS])
 
+        plt.title("DeterministicTournamentSelection")
+        plt.xlabel("Generation")
+        plt.ylabel("Fitness")
+        plt.show()
         plt.savefig(OUTPUT_DIR + "linegraph.jpg")
 
 
@@ -77,4 +90,3 @@ if __name__ == "__main__":
         os.mkdir(OUTPUT_DIR)
 
     Plotter().plot()
-
